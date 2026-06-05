5b1554c28fc516d954a03ff71bce003c504b7cad
Kammsk87 <tmn26ggvm4@privaterelay.appleid.com>
2026-06-05 18:16:19 +0500

    Configure Botalin Supabase key

--- share/strategy-lab-7q4m2v/remote-journal-config.js
+++ share/strategy-lab-7q4m2v/remote-journal-config.js
@@ -1,5 +1,5 @@
 window.BOTALIN_REMOTE_JOURNAL_CONFIG = window.BOTALIN_REMOTE_JOURNAL_CONFIG || {
   url: "https://dcpenxsthdhvhhqgvgjq.supabase.co",
-  anonKey: "",
+  anonKey: "sb_publishable_BYYOhjwhgjZBP27Yw7YkVg_CEhF6ugc",
   table: "crypto_strategy_trades"
 };
